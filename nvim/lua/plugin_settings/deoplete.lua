c3dab809f0e6521e991759f7080fd4809e44d426
Shirasaka <tk.shirasaka@gmail.com>
2020-11-15 00:57:46 +0900

    Update

--- nvim/lua/plugin_settings/deoplete.lua
+++ nvim/lua/plugin_settings/deoplete.lua
@@ -3,5 +3,6 @@ vim.call('deoplete#enable')
 vim.call('deoplete#custom#option', {
     ignore_case = true,
     ignore_sources = { ['_'] =  { 'around', 'file', 'member', 'omni' } },
+    nofile_complete_filetypes = { 'denite-filter', 'zsh' }
 })
 vim.call('deoplete#custom#option', 'sources', { ['denite-filter'] = { 'denite' } })
--- nvim/lua/plugin_settings/nvim-lspconfig.lua
+++ nvim/lua/plugin_settings/nvim-lspconfig.lua
@@ -1,20 +1,20 @@
 -- Configs
 
 local nvim_lsp = require'nvim_lsp'
-local diagnostic = require'diagnostic'
-local util = nvim_lsp.util
 
-util.default_config = vim.tbl_extend('force', util.default_config, { on_attach=diagnostic.on_attach })
-
-nvim_lsp.bashls.setup {  }
-nvim_lsp.sumneko_lua.setup{ cmd = { 'lua-language-server' } }
-nvim_lsp.vimls.setup {  }
-nvim_lsp.pyls_ms.setup {  }
-nvim_lsp.gopls.setup {  }
-nvim_lsp.intelephense.setup {  }
-nvim_lsp.tsserver.setup {  }
-nvim_lsp.dockerls.setup {  }
-nvim_lsp.html.setup {  }
-nvim_lsp.cssls.setup {  }
-nvim_lsp.jsonls.setup {  }
-nvim_lsp.yamlls.setup {  }
+nvim_lsp.bashls.setup({})
+nvim_lsp.clangd.setup({})
+nvim_lsp.sumneko_lua.setup({
+    settings = { Lua = { diagnostics = { enable = true, globals = { "vim" } } } },
+    cmd = 'lua-language-server'
+})
+nvim_lsp.vimls.setup({})
+nvim_lsp.pyls_ms.setup({})
+nvim_lsp.gopls.setup({})
+nvim_lsp.intelephense.setup({})
+nvim_lsp.tsserver.setup({})
+nvim_lsp.dockerls.setup({})
+nvim_lsp.html.setup({})
+nvim_lsp.cssls.setup({})
+nvim_lsp.jsonls.setup({})
+nvim_lsp.yamlls.setup({})
