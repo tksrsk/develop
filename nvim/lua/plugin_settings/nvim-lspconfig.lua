37efcc08cf2d22a6049067d0800f2e0f42a7d775
shirasaka <shirasaka@n-create.co.jp>
2020-11-16 19:44:11 +0900

    Update

--- nvim/lua/plugin_settings/nvim-lspconfig.lua
+++ nvim/lua/plugin_settings/nvim-lspconfig.lua
@@ -1,20 +1,20 @@
 -- Configs
 
-local nvim_lsp = require'nvim_lsp'
+local lspconfig = require'lspconfig'
 
-nvim_lsp.bashls.setup({})
-nvim_lsp.clangd.setup({})
-nvim_lsp.sumneko_lua.setup({
+lspconfig.bashls.setup({})
+lspconfig.clangd.setup({})
+lspconfig.sumneko_lua.setup({
     settings = { Lua = { diagnostics = { enable = true, globals = { "vim" } } } },
     cmd = 'lua-language-server'
 })
-nvim_lsp.vimls.setup({})
-nvim_lsp.pyls_ms.setup({})
-nvim_lsp.gopls.setup({})
-nvim_lsp.intelephense.setup({})
-nvim_lsp.tsserver.setup({})
-nvim_lsp.dockerls.setup({})
-nvim_lsp.html.setup({})
-nvim_lsp.cssls.setup({})
-nvim_lsp.jsonls.setup({})
-nvim_lsp.yamlls.setup({})
+lspconfig.vimls.setup({})
+lspconfig.pyls_ms.setup({})
+lspconfig.gopls.setup({})
+lspconfig.intelephense.setup({})
+lspconfig.tsserver.setup({})
+lspconfig.dockerls.setup({})
+lspconfig.html.setup({})
+lspconfig.cssls.setup({})
+lspconfig.jsonls.setup({})
+lspconfig.yamlls.setup({})
