ab5d6af698fd8e3ba63e8ea6adb04da3b6987314
Shirasaka <tk.shirasaka@gmail.com>
2021-04-11 22:54:14 +0900

    Update

--- nvim/lua/plugin_settings/nvim-tree.lua
+++ nvim/lua/plugin_settings/nvim-tree.lua
@@ -4,9 +4,10 @@ vim.cmd([[
 ]])
 
 -- Configs
-vim.g.nvim_tree_ignore = { 'node_modules', 'vendor', 'Vendor' }
+vim.g.nvim_tree_gitignore = 1
 vim.g.nvim_tree_git_hl = 1
 vim.g.nvim_tree_follow = 1
 vim.g.nvim_tree_auto_close = 1
 vim.g.nvim_tree_hide_dotfiles = 1
 vim.g.nvim_tree_indent_markers = 1
+vim.g.nvim_tree_lsp_diagnostics = 1
