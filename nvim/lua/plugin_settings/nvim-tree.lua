c232b5ca757b564f09b83c1beb16691e6ac0a468
shirasaka <shirasaka@n-create.co.jp>
2021-04-19 10:30:48 +0900

    Update

--- nvim/lua/plugin_settings/nvim-tree.lua
+++ nvim/lua/plugin_settings/nvim-tree.lua
@@ -12,3 +12,4 @@ vim.g.nvim_tree_auto_close = 1
 vim.g.nvim_tree_hide_dotfiles = 1
 vim.g.nvim_tree_indent_markers = 1
 vim.g.nvim_tree_lsp_diagnostics = 1
+vim.g.nvim_tree_highlight_opened_files = 1
