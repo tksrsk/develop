743712bbd437996018f3da005d5be54cbb8d498c
shirasaka <shirasaka@n-create.co.jp>
2021-06-08 10:32:15 +0900

    Update

--- nvim/lua/plugin_settings/nvim-tree.lua
+++ nvim/lua/plugin_settings/nvim-tree.lua
@@ -14,3 +14,4 @@ vim.g.nvim_tree_indent_markers = 1
 vim.g.nvim_tree_lsp_diagnostics = 1
 vim.g.nvim_tree_highlight_opened_files = 1
 vim.g.nvim_tree_hijack_cursor = 1
+vim.g.nvim_tree_update_cwd = 1
