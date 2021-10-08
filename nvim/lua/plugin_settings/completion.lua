7988664f12ae87dc90001303d05759c507469d91
shirasaka <shirasaka@n-create.co.jp>
2021-10-08 09:20:51 +0900

    Update

--- nvim/lua/plugin_settings/completion.lua
+++ nvim/lua/plugin_settings/completion.lua
@@ -2,17 +2,17 @@
 require('cmp').setup({
     snippet = {
       expand = function(args)
-        vim.fn["vsnip#anonymous"](args.body)
+        vim.fn['vsnip#anonymous'](args.body)
       end,
     },
     formatting = {
         format = require('lspkind').cmp_format({ with_text = false })
     },
     sources = {
-        { name = "nvim_lsp" };
-        { name = "nvim_lua" };
-        { name = "vim-dadbod-completion" },
-        { name = "path" };
-        { name = "buffer" };
+        { name = 'nvim_lsp' },
+        { name = 'nvim_lua' },
+        { name = 'vim-dadbod-completion' },
+        { name = 'path' };
+        { name = 'buffer' };
     }
 })
