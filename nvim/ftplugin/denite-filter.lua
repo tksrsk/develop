f311d1ab1395b8862571d035b4b8f4b2c83b6adb
shirasaka <shirasaka@n-create.co.jp>
2021-11-16 14:48:25 +0900

    Update

---
+++ nvim/ftplugin/denite-filter.lua
@@ -0,0 +1,19 @@
+require('cmp').setup.buffer({
+    sources = {
+        {
+            name = 'buffer',
+            opts = {
+                keyword_pattern = [[\%(-\?\d\+\%(\.\d\+\)\?\|\h\w*\%([\-\/]\w*\)*\)]],
+                get_bufnrs = function ()
+                    for _, win in ipairs(vim.api.nvim_list_wins()) do
+                        local buf = vim.api.nvim_win_get_buf(win)
+                        if vim.api.nvim_buf_get_option(buf, 'filetype') == 'denite' then
+                            return { buf }
+                        end
+                    end
+                    return {}
+                end,
+            }
+        },
+    },
+})
--- nvim/lua/plugin_settings/completion.lua
+++ nvim/lua/plugin_settings/completion.lua
@@ -17,5 +17,6 @@ require('cmp').setup({
     },
     experimental = {
         native_menu = true,
+        ghost_text = true,
     },
 })
