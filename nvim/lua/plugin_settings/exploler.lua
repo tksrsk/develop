041b252c099f1c2ee9ea02ffcb9aa19dd2d60a6a
shirasaka <shirasaka@n-create.co.jp>
2021-12-17 19:10:44 +0900

    Update

--- nvim/lua/plugin_settings/exploler.lua
+++ nvim/lua/plugin_settings/exploler.lua
@@ -11,7 +11,6 @@ require('nvim-tree').setup({
     diagnostics = { enable = true },
     update_focused_file = {
         enable = true,
-        update_cwd = true
     },
     filters = { dotfiles = true },
 })
