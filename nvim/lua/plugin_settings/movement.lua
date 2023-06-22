537f68e20309f047949f57d18f769c6df34f3709
shirasaka <tk.shirasaka@gmail>
2023-06-23 00:58:24 +0900

    Update

--- nvim/lua/plugin_settings/movement.lua
+++ nvim/lua/plugin_settings/movement.lua
@@ -3,8 +3,11 @@ vim.keymap.set('n', 'j', '<Plug>(accelerated_jk_gj)')
 vim.keymap.set('n', 'k', '<Plug>(accelerated_jk_gk)')
 
 -- Hop
-require('flash').setup({
-    search = { mode = 'fuzzy' }
-})
-vim.keymap.set({'n', 'x', 'o'}, '<leader>/', require('flash').jump)
+require('flash').setup()
+vim.keymap.set({'n', 'x', 'o'}, '<leader>/', function ()
+    require('flash').jump({ search = { mode = 'fuzzy' } })
+end)
+vim.keymap.set({'n', 'x', 'o'}, '<leader>j', function ()
+    require('flash').jump({ search = { mode = 'search' }, pattern = '^' })
+end)
 vim.keymap.set({'v', 'o'}, 'm', require('flash').treesitter)
