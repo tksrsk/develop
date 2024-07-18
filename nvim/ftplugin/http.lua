7c4735110bc07cb8621a25db13427163fa165bd9
shirasaka <shirasaka@n-create.co.jp>
2024-07-18 11:23:10 +0900

    Update

--- nvim/ftplugin/http.lua
+++ nvim/ftplugin/http.lua
@@ -1,3 +1,4 @@
-vim.keymap.set('n', '<cr>', '<cmd>lua require("kulala").run()<cr>', { silent = true, buffer = 0 })
-vim.keymap.set('n', '<2-leftmouse>', '<cmd>lua require("kulala").run()<cr>', { silent = true, buffer = 0 })
-vim.keymap.set('n', '<tab>', '<cmd>lua require("kulala").toggle_view()<cr>', { silent = true, buffer = 0 })
+vim.keymap.set('n', '<cr>', function() require('kulala').run() end, { silent = true, buffer = 0 })
+vim.keymap.set('n', '<c-p>', function() require('kulala').copy() end, { silent = true, buffer = 0 })
+vim.keymap.set('n', '<2-leftmouse>', function() require('kulala').run() end, { silent = true, buffer = 0 })
+vim.keymap.set('n', '<tab>', function() require('kulala').toggle_view() end, { silent = true, buffer = 0 })
--- nvim/lua/plugin_settings/terminal.lua
+++ nvim/lua/plugin_settings/terminal.lua
@@ -1,6 +1,5 @@
 -- Toggleterm
 require('toggleterm').setup({
     shell = 'fish',
-    open_mapping = '<c-t>',
     winbar = { enabled = true },
 })
