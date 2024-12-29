cd47f04a7eaa745e5ea825fb4f2aa348ea4f33f4
shirasaka <shirasaka@n-create.co.jp>
2024-12-29 22:24:58 +0900

    Update

--- nvim/lua/plugin_settings/dbms.lua
+++ nvim/lua/plugin_settings/dbms.lua
@@ -8,12 +8,16 @@ require('dbee').api.core.register_event_listener('current_connection_changed', f
     local conn = require('dbee').api.core.get_current_connection()
     local driver = conn.type == 'sqlserver' and 'mssql' or conn.type
     local connections = { { driver = driver, dataSourceName = conn.url } }
-    local old_clients = require('lspconfig').sqls.manager:clients()
+    local old_clients = vim.lsp.get_clients({ name = 'sqls' })
 
-    require('lspconfig').sqls.setup({
-        settings = { sqls = { connections = connections } },
-        on_attach = function (_, bufnr)
-            if #old_clients > 0 then vim.lsp.completion.enable(false, old_clients[1].id, bufnr) end
-        end
-    })
+    if driver ~= 'redis' then
+        require('lspconfig').sqls.setup({
+            settings = { sqls = { connections = connections } },
+            on_attach = function (_, bufnr)
+                for _, old_client in pairs(old_clients) do
+                    vim.lsp.completion.enable(false, old_clients[1].id, bufnr)
+                end
+            end
+        })
+    end
 end)
