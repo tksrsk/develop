71b7fdcc8a1787d476a55d39ec2f8acd79c2e519
shirasaka <tk.shirasaka@gmail>
2026-02-12 02:10:30 +0900

    Update

--- nvim/lua/plugin_settings/fuzzy-finder.lua
+++ nvim/lua/plugin_settings/fuzzy-finder.lua
@@ -1,5 +1,18 @@
 -- Telescope
+local attach_mappings = function(_, map)
+    map('i', '<C-a>', function()
+        local entry = require('telescope.actions.state').get_selected_entry()
+        if entry then envim_acp_add_file(entry.path or entry.value) end
+    end)
+    return true
+end
+
 require('telescope').setup({
+    pickers = {
+        find_files = { attach_mappings = attach_mappings },
+        git_status = { attach_mappings = attach_mappings },
+        git_files = { attach_mappings = attach_mappings },
+    },
     extensions = {
         fzf = {
             fazzy = true,
